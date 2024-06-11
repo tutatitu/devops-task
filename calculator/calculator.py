c3587b5cbb6932ee1e8a90992585d6a7bec45fe0
Kirill Lartsev <lartsev200@yandex.ru>
2024-06-11 19:49:12 +0300

    update calculator.py, bug fix

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -4,3 +4,10 @@ def add(x, y):
 
 def subtract(x, y):
     return x - y
+
+
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+
+print(a, "+", b, "=", add(int(a), int(b)))
+print(a, "-", b, "=", subtract(int(a), int(b)))
