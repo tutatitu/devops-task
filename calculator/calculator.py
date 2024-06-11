4996b62d2add917e61ed8d2d59700e761d579495
Kirill Lartsev <lartsev200@yandex.ru>
2024-06-11 20:10:46 +0300

    bug fix

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -6,8 +6,13 @@ def subtract(x, y):
     return x - y
 
 
-a = input("Enter first number: ")
-b = input("Enter second number: ")
+def main():
+    a = input("Enter first number: ")
+    b = input("Enter second number: ")
 
-print(a, "+", b, "=", add(int(a), int(b)))
-print(a, "-", b, "=", subtract(int(a), int(b)))
+    print(a, "+", b, "=", add(int(a), int(b)))
+    print(a, "-", b, "=", subtract(int(a), int(b)))
+
+
+if __name__ == "__main__":
+    main()
